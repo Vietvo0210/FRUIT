d6489d3bd3c02d5b52c1a453eb917e50133f768d
Quoc Viet <vietvmq@smartgift.vn>
2023-02-17 10:29:58 +0700

    update menu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,88 @@
 <template>
-  <div>
-    <a-button type="primary">Primary Button</a-button>
-    <a-button>Default Button</a-button>
-    <a-button type="dashed">Dashed Button</a-button>
+
+  <div class="thisIsMenu">
+    <a-switch
+      :checked="theme === 'dark'"
+      checked-children="Dark"
+      un-checked-children="Light"
+      @change="changeTheme"
+    />
+    <br />
+    <br />
+    <a-menu
+      v-model:openKeys="openKeys"
+      v-model:selectedKeys="selectedKeys"
+      mode="inline"
+      :theme="theme"
+    >
+      <a-menu-item key="1">
+        <template #icon>
+          <MailOutlined />
+        </template>
+        Navigation One
+      </a-menu-item>
+      <a-menu-item key="2">
+        <template #icon>
+          <CalendarOutlined />
+        </template>
+        Navigation Two
+      </a-menu-item>
+      <a-sub-menu key="sub1">
+        <template #icon>
+          <AppstoreOutlined />
+        </template>
+        <template #title>Navigation Three</template>
+        <a-menu-item key="3">Option 3</a-menu-item>
+        <a-menu-item key="4">Option 4</a-menu-item>
+        <a-sub-menu key="sub1-2" title="Submenu">
+          <a-menu-item key="5">Option 5</a-menu-item>
+          <a-menu-item key="6">Option 6</a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <template #icon>
+          <SettingOutlined />
+        </template>
+        <template #title>Navigation Four</template>
+        <a-menu-item key="7">Option 7</a-menu-item>
+        <a-menu-item key="8">Option 8</a-menu-item>
+        <a-menu-item key="9">Option 9</a-menu-item>
+        <a-menu-item key="10">Option 10</a-menu-item>
+      </a-sub-menu>
+    </a-menu>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+import { defineComponent, reactive, toRefs } from 'vue';
+import { MailOutlined, CalendarOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+export default defineComponent({
+  components: {
+    MailOutlined,
+    CalendarOutlined,
+    AppstoreOutlined,
+    SettingOutlined,
+  },
+  setup() {
+    const state = reactive({
+      theme: 'dark',
+      selectedKeys: ['1'],
+      openKeys: ['sub1'],
+    });
+    const changeTheme = checked => {
+      state.theme = checked ? 'dark' : 'light';
+    };
+    return {
+      ...toRefs(state),
+      changeTheme,
+    };
+  },
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.thisIsMenu{
+  position: relative;
+  left: 100px;
+  width: 250px;
 }
 </style>
