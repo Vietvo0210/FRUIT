50345bf53ce01dd94a0008755b9086f724367a1c
Vietvmq <quocviet@jv-it.com.vn>
2024-10-11 10:42:22 +0700

    Commit: Tuyentap nha anh trong mit thai

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -7,48 +7,51 @@
       </template>
       Navigation One
     </a-menu-item>
-    <a-sub-menu key="sub1">
-      <template #icon>
-        <setting-outlined />
-      </template>
-      <template #title>Navigation Three - Submenu</template>
-      <a-menu-item-group title="Item 1">
-        <a-menu-item key="setting:1">Option 1</a-menu-item>
-        <a-menu-item key="setting:2">Option 2</a-menu-item>
-      </a-menu-item-group>
-      <a-menu-item-group title="Item 2">
-        <a-menu-item key="setting:3">Option 3</a-menu-item>
-        <a-menu-item key="setting:4">Option 4</a-menu-item>
-      </a-menu-item-group>
-    </a-sub-menu>
     <a-menu-item key="alipay">
       <a href="https://www.facebook.com/ninjanguyenhoanganhhh" target="_blank" rel="noopener noreferrer">
         Tôi là VIỆT ANH ĐỖ
       </a>
     </a-menu-item>
   </a-menu>
-    <div>
+    <div style="padding: 20px;">
+      <h1> Tuyển tập "Um, nhà anh trồng mít thái"</h1> <div style="display: flex">
       <a style="margin: 20px">hình tôi đỉnh</a>
       <img
-        style="width: 400px; height: 400px;"
-        src="../img/vietanhdo_1.jpg"
+          style="width: 400px; height: 400px;"
+          src="../img/vietanhdo_1.jpg"
       />
       <br/>
       <a style="margin: 20px" >hình tôi cực đỉnh</a>
       <img
-        style="width: 400px; height: 400px;"
-        src="../img/vietanhdo_2.jpg"
+          style="width: 400px; height: 400px;"
+          src="../img/vietanhdo_2.jpg"
       />
     </div>
+      <div style="display: flex">
+        <a style="margin: 20px">Mít đặc</a>
+        <img
+            style="width: 400px; height: 400px;"
+            src="../img/Vad_3.jpg"
+        />
+        <br/>
+        <a style="margin: 20px" >OK, i dont know</a>
+        <img
+            style="width: 400px; height: 400px;"
+            src="../img/Dc.jpg"
+        />
+      </div>
+    </div>
+    
+   
+    
   </div>
 </template>
 <script>
 import { defineComponent, ref } from 'vue';
-import { MailOutlined, SettingOutlined } from '@ant-design/icons-vue';
+import { MailOutlined } from '@ant-design/icons-vue';
 export default defineComponent({
   components: {
     MailOutlined,
-    SettingOutlined,
   },
   setup() {
     const current = ref(['mail']);
